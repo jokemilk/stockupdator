514dc1a339ceac2bff4812d203373729c77c3290
jokemilk <joemilu@gmail.com>
2014-09-26 21:15:06 +0800

    refact query
    add limit to client count
    add limit to max subscribe count
    
    	modified:   TODO
    	modified:   client_mail_handler.py
    	modified:   database.py

--- client_mail_handler.py
+++ client_mail_handler.py
@@ -10,6 +10,9 @@ import logging
 
 root_key = ndb.Key('client_db', 'root')
 
+MAX_STOCK_CNT = 16
+MAX_CLIENT_CNT = 1000
+
 #handle mail request and do replay
 
 #in request we can do
@@ -19,40 +22,47 @@ root_key = ndb.Key('client_db', 'root')
 #4.delete the account/if there is no subed stock, delete it also
 
 def listall(client):
-    records = client_db.query(client_db.mail == client,ancestor=root_key).fetch()
-    if len(records) == 1:
-        return "client:"+client+"; stock list: "+records[0].stocklist
+    record = client_db.query(client_db.mail == client,ancestor=root_key).get()
+    if record is not None:
+        return "client:"+client+"; stock list: "+record.stocklist
     else:
         logging.error("listall error: client not exist")
         return None
 
 def addstock(client,stock):
-    records = client_db.query(client_db.mail == client,ancestor=root_key).fetch()
-    if len(records) == 1:
-        if stock in records[0].stocklist:
+    record = client_db.query(client_db.mail == client,ancestor=root_key).get()
+    if record is not None:
+        if stock in record.stocklist:
             logging.warn("addstock warning: stock already exist in list")
             return
         else:
-            records[0].stocklist = records[0].stocklist + ' ' + stock
-            records[0].put()
+            if record.stockcnt >= MAX_STOCK_CNT:
+                logging.error('client can only subscribe %d stocks' %(MAX_STOCK_CNT))
+                return
+            record.stocklist = record.stocklist + ' ' + stock
+            record.stockcnt += 1
+            record.put()
             logging.info("client %s add stock %s" %(client,stock))
-    elif len(records) == 0:
-        records = client_db(parent=root_key,mail = client,stocklist = stock)
-        res=records.put()
     else:
-        logging.error("addstock error: client not exist or dup")
+        if client_db.query(ancestor=root_key).count() >= MAX_CLIENT_CNT:
+            logging.error('Reach Max client number %d' %(MAX_CLIENT_CNT))
+            return
+        new_client = client_db(parent=root_key,mail = client,stockcnt = 1,stocklist = stock)
+        res=new_client.put()
+        logging.info("new client %s: add stock %s" %(new_client.mail,new_client.stocklist))
 
 def rmstock(client,stock):
-    records = client_db.query(client_db.mail == client,ancestor=root_key).fetch()
-    if len(records) == 1:
-        if stock in records[0].stocklist:
-            ls = records[0].stocklist.split()
+    record = client_db.query(client_db.mail == client,ancestor=root_key).get()
+    if record is not None:
+        if stock in record.stocklist:
+            ls = record.stocklist.split()
             ls.remove(stock)
             if 0 == len(ls):
-                records[0].key.delete()
+                record.key.delete()
                 return
-            records[0].stocklist = ' '.join(ls)
-            records[0].put()
+            record.stockcnt -= 1
+            record.stocklist = ' '.join(ls)
+            record.put()
             logging.info("client %s rm stock %s" %(client,stock))
         else:
             logging.warn("rmstock warning: stock doesn't exist in list")
@@ -60,9 +70,9 @@ def rmstock(client,stock):
         logging.error("rmstock error: client not exist")
 
 def unsub(client):
-    records = client_db.query(client_db.mail == client,ancestor=root_key).fetch()
-    if len(records) == 1:
-        records[0].key.delete()
+    record = client_db.query(client_db.mail == client,ancestor=root_key).get()
+    if record is not None:
+        record.key.delete()
         logging.info("client %s deleted" %(client))
     else:
         logging.error("unsub error: client not exist")
--- database.py
+++ database.py
@@ -2,4 +2,5 @@ from google.appengine.ext import ndb
 
 class client_db(ndb.Model):
     mail = ndb.StringProperty(required=True)
+    stockcnt = ndb.IntegerProperty()
     stocklist = ndb.TextProperty()
